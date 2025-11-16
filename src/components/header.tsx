0ca2c4c628cdcdccf840b3847c28e2c4724b937f
Mtseng <mtseng36@gmail.com>
2025-11-16 19:02:34 +0500

    theme toggle

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { RefreshCw } from "lucide-react";
 import type { Player } from "@/types/types";
 import { PresetsDialog } from "./presetChooser";
+import { ModeToggle } from "./theme-toggle";
 
 interface PresetsHeaderProps {
   currentPlayers: Player[];
@@ -21,8 +22,10 @@ export function PresetsHeader({
     <header className="sticky top-0 z-50 md:w-[85vw] w-full mx-auto border-b border-border/40 bg-background shadow-md rounded-xl">
       <div className="container mx-auto flex h-16 items-center justify-between px-6">
         <h1 className="text-2xl text-muted-foreground font-bold tracking-tight transition-all hover:scale-105 hover:text-primary">HLStats Parser</h1>
+        
 
         <div className="flex items-center gap-3">
+          
           <Button
             variant="outline"
             onClick={onRefresh}
@@ -34,6 +37,7 @@ export function PresetsHeader({
             Refresh
           </Button>
           <PresetsDialog />
+          <ModeToggle/>
         </div>
       </div>
     </header>
---
+++ src/components/theme-toggle.tsx
@@ -0,0 +1,24 @@
+import { Moon, Sun } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import { useTheme } from "@/components/theme-provider"
+
+export function ModeToggle() {
+  const { setTheme, theme } = useTheme()
+
+  const changeTheme = () =>{
+    if (theme==="dark")
+        setTheme("light");
+    else
+        setTheme("dark")
+  }
+
+
+  return (
+        <Button variant="outline" size="icon" onClick={(e)=> {e.stopPropagation(); changeTheme()}}>    
+          {theme === "light" && <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90"/>}
+          {theme === "dark" && <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0"/>}
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+  )
+}
